46bd39356d4d6e6370c50d13b872ff86df89067d
SrKomodo <komodosr@gmail.com>
2017-10-12 20:49:51 -0300

    rework background script to handle interval changes

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
 chrome.storage.sync.get({"interval": 5}, items => {
   chrome.alarms.create("RSSFetch", {periodInMinutes: items.interval});
+  chrome.alarms.onAlarm.addListener(fetchFeeds);
 });
 
-chrome.alarms.onAlarm.addListener(alarm => {
-  console.log("I'm supposed to fetch the RSS feeds now!");
+chrome.storage.onChanged.addListener(changes => {
+  if (changes.interval) {
+    chrome.alarms.clear("RSSFetch");
+    chrome.alarms.create("RSSFetch", {periodInMinutes: changes.interval.newValue });
+    chrome.alarms.onAlarm.addListener(fetchFeeds);
+  }
 });
+
+function fetchFeeds(alarm) {
+  console.log("I'm supposed to fetch the RSS feeds now!");
+}
