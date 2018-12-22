f71ebb0033160a23f0e45931aad5b33f1469f6e2
Sr Komodo <komodosr@gmail.com>
2018-12-22 03:24:49 -0300

    tweaks to manage screen

--- src/manage/manage.ts
+++ src/manage/manage.ts
@@ -7,23 +7,27 @@ const saveSettings = document.getElementById("saveSettings")!;
 const feedTemplate = document.getElementById("feed") as HTMLTemplateElement;
 const feedsEl = document.getElementById("feeds")!;
 
+const minutes = (s: string) => `${s} ${s === "1" ? "minute" : "minutes"}`;
+
 saveSettings.addEventListener("click", () => {
   browser.storage.sync.set({ interval: parseInt(interval.value, 10) });
 });
 
 interval.addEventListener("input", () => {
-  intervalOutput.textContent = `${interval.value} ${
-    interval.value === "1" ? "minute" : "minutes"
-  }`;
+  intervalOutput.textContent = minutes(interval.value);
 });
 
 browser.storage.sync.get({ interval: 5, feeds: [] }).then(results => {
   interval.value = results.interval.toString();
-  intervalOutput.textContent = `${results.interval} ${
-    results.interval === 1 ? "minute" : "minutes"
-  }`;
+  intervalOutput.textContent = minutes(results.interval.toString());
 
   const feeds: Feed[] = results.feeds;
+  if (feeds.length === 0) {
+    const text = document.createElement("div");
+    text.className = "no_feeds";
+    text.textContent = "You have no feeds. Go find some!";
+    feedsEl.appendChild(text);
+  }
   for (const feed of feeds) {
     const el = document.importNode(feedTemplate.content, true);
     (el.querySelector(".name") as HTMLInputElement).value = feed.name;
