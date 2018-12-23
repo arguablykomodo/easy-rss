7f92e8e6ce81eaaf32606ef1720adc5945e9d080
Sr Komodo <komodosr@gmail.com>
2018-12-23 18:15:34 -0300

    add option to sync youtube subscriptions

--- src/manage/manage.ts
+++ src/manage/manage.ts
@@ -35,3 +35,33 @@ browser.storage.sync.get({ interval: 5, feeds: [] }).then(results => {
     feedsEl.appendChild(el);
   }
 });
+
+document.getElementById("sync")!.addEventListener("click", async () => {
+  const r = await fetch(
+    "https://www.youtube.com/subscription_manager?action_takeout=1"
+  );
+
+  if (r.redirected) alert("Please login into YouTube to sync subscriptions");
+  else {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(await r.text(), "application/xml");
+    const newFeeds: Feed[] = [];
+    for (const node of xml.querySelectorAll("outline > outline")) {
+      newFeeds.push({
+        name: node.getAttribute("title")!,
+        url: node.getAttribute("xmlUrl")!
+      });
+    }
+
+    const feeds: Feed[] = (await browser.storage.sync.get({ feeds: [] })).feeds;
+
+    // Remove existing youtube subs
+    let i = feeds.length;
+    while (i--)
+      if (feeds[i].url.startsWith("https://www.youtube.com/feeds/videos.xml"))
+        feeds.splice(i, 1);
+
+    await browser.storage.sync.set({ feeds: feeds.concat(newFeeds) as any });
+    location.reload();
+  }
+});
